29b1fe9d1d380f092cfbf951571e52e64e748dbf
Tommy-LSA <pirat@stadtweg.de>
2018-10-12 09:31:47 +0200

    split getting nodes from http request to avoid lagging

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const listenport = 8089;
+const listenport = 8089; // port the service listen to requests
+const interval = 30; // seconds to repeatly get nodes from hopglass server
+
 const http = require('http');
 const url = require('url');
 const https = require('https');
+
 const options = {
     hostname: 'freifunk-halle.org',
     port: 443,
@@ -9,10 +12,12 @@ const options = {
     method: 'GET'
 };
 
-http.createServer((req, res) => {
-    var query = url.parse(req.url, true).query;
-    var retval = {error:null, result:[]};
+var nodelist = {nodes:[]};
+getNodes();
 
+setInterval(function(){ getNodes(); }, interval * 1000);
+
+function getNodes(){
     const requ = https.request(options, (resp) => {
         let content = "";
         resp.on('data', (d) => {
@@ -20,21 +25,23 @@ http.createServer((req, res) => {
         });
 
         resp.on('end',()=>{
-            let nodelist = JSON.parse(content);
-            nodelist.nodes.forEach(element => {
-                if(element.nodeinfo.node_id === query.nodeid){
-                    retval.result.push(element);
-                }
-            });
-            res.end(JSON.stringify(retval));
+            nodelist = JSON.parse(content);
         })
     });
       
-    requ.on('error', (e) => {
-        retval.error = e.message;
-        res.end(JSON.stringify(retval));
-    });
-
+    requ.on('error', (e) => { });
     requ.end();
+}
+
+http.createServer((req, res) => {
+    var query = url.parse(req.url, true).query;
+    var retval = {error:null, result:[]};
+
+    nodelist.nodes.forEach(element => {
+        if(element.nodeinfo.node_id === query.nodeid){
+            retval.result.push(element);
+        }
+    });
+    res.end(JSON.stringify(retval));
 
 }).listen(listenport);
